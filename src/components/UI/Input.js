f3c47348e5732b3890ec5d880c2dd83326f87696
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2022-08-02 14:18:35 +0700

    add input

---
+++ src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import clasess from './Input.module.css';
+
+const Input = (props) => {
+  return (
+    <div className={clasess.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
