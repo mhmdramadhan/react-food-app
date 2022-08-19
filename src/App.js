57f38f144aac5e6ab3d96a7c989f4237ba4da5d3
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2022-08-19 16:33:06 +0700

    add data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 // import './App.css';
 
 function App() {
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={shownCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
