877794a37402b6bfc0e8099b7293102ea06ca298
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2022-08-24 16:17:59 +0700

    add data

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
+
 import clasess from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={clasess.button} onClick={props.onClick}>
       <span className={clasess.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={clasess.badge}>
-          3
-      </span>
+      <span className={clasess.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
